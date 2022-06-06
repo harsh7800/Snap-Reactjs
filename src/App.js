dc4d06d557b86287ac8e892970e741e221a54b68
Harsh shinde <harshshinde5606@gmail.com>
2022-06-07 01:10:56 +0530

    moved routes from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import Navbar from './components/Navbar/Navbar';
 import React   from 'react'
 import './App.scss'
-import { Route, Routes } from 'react-router-dom';
-import Todo from './components/todo/Todo'
-import Calendar from './components/calendar/DatePicker'
-import Reminder from './components/reminder/Reminder'
-import Stopwatch from './components/Stopwatch/Stopwatch'
 import Section from './components/main-Section/Section';
-import Error from './Error';
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Section/>}/>
-        <Route path='/todo-list' element={<Todo/>}/>
-        <Route path='/calendar' element={<Calendar/>}/>
-        <Route path='/reminder' element={<Reminder/>}/>
-        <Route path='/stopwatch' element={<Stopwatch />} />
-        <Route path='/History' element={<Error/>}/>
-        <Route path='/Our-Team' element={<Error/>}/>
-        <Route path='/Blog' element={<Error/>}/>
-        <Route path='/Career' element={<Error/>}/>
-        <Route path='/About' element={<Error/>}/>
-        <Route path='/Home' element={<Section/>}/>
-      </Routes>
+      <Section/>      
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter} from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Todo from "./components/todo/Todo";
+import Calendar from "./components/calendar/DatePicker";
+import Reminder from "./components/reminder/Reminder";
+import Stopwatch from "./components/Stopwatch/Stopwatch";
+import Error from "./Error";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App/>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/todo-list" element={<Todo />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/reminder" element={<Reminder />} />
+      <Route path="/stopwatch" element={<Stopwatch />} />
+      <Route path="/History" element={<Error />} />
+      <Route path="/Our-Team" element={<Error />} />
+      <Route path="/Blog" element={<Error />} />
+      <Route path="/Career" element={<Error />} />
+      <Route path="/About" element={<Error />} />
+      <Route path="/Home" element={<App />} />
+    </Routes>
   </BrowserRouter>
 );
