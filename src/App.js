62be90be72c70c24b6dd5e5756f8cb981d3953bd
Harsh shinde <harshshinde5606@gmail.com>
2022-06-07 01:42:51 +0530

    Moved route back into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import Navbar from './components/Navbar/Navbar';
 import React   from 'react'
 import './App.scss'
+import { Route, Routes } from "react-router-dom";
 import Section from './components/main-Section/Section';
+import Todo from "./components/todo/Todo";
+import Calendar from "./components/calendar/DatePicker";
+import Reminder from "./components/reminder/Reminder";
+import Stopwatch from "./components/Stopwatch/Stopwatch";
+import Error from "./Error";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/">
+          <Section />
+        </Route>
+        <Route path='/Todo-List'><Todo/></Route>
+        <Route path='/Calendar'><Calendar/></Route>
+        <Route path='/Reminder'><Reminder/></Route>
+        <Route path='/Stopwatch'><Stopwatch/></Route>
+        <Route path='/History'><Error/></Route>
+        <Route path='/Our-Team'><Error/></Route>
+        <Route path='/Blog'><Error/></Route>
+        <Route path='/Career'><Error/></Route>
+        <Route path='/About'><Error/></Route>
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter} from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
-import Todo from "./components/todo/Todo";
-import Calendar from "./components/calendar/DatePicker";
-import Reminder from "./components/reminder/Reminder";
-import Stopwatch from "./components/Stopwatch/Stopwatch";
-import Error from "./Error";
-import Section from './components/main-Section/Section';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <StrictMode>
     <App/>
-    <Routes>
-      <Route  path='/'><Section/></Route>
-      <Route exact path="/todo-list" element={<Todo />} />
-      <Route exact path="/calendar" element={<Calendar />} />
-      <Route exact path="/reminder" element={<Reminder />} />
-      <Route exact path="/stopwatch" element={<Stopwatch />} />
-      <Route exact path="/History" element={<Error />} />
-      <Route exact path="/Our-Team" element={<Error />} />
-      <Route exact path="/Blog" element={<Error />} />
-      <Route exact path="/Career" element={<Error />} />
-      <Route exact path="/About" element={<Error />} />
-      <Route exact path="/Home" element={<Section/>} />
-    </Routes>
+    </StrictMode>
   </BrowserRouter>
 );
